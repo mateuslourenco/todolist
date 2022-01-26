19a6ffdcfa54faae74e62f4664b53a7d99f28f23
mateus <mateuslourenco55@outlook.com>
2022-01-25 22:47:34 -0300

    adicionado listagem de tarefas pendentes
    close #4

--- webdev/tarefas/tests/test_tarefas_get.py
+++ webdev/tarefas/tests/test_tarefas_get.py
@@ -2,6 +2,8 @@ import pytest
 from django.urls import reverse
 from pytest_django.asserts import assertContains
 
+from webdev.tarefas.models import Tarefa
+
 
 @pytest.fixture
 def resp(client):
@@ -18,3 +20,24 @@ def test_formulario_presente(client, resp):
 
 def test_botao_salvar_presente(client, resp):
     assertContains(resp, '<button type="submit"')
+
+
+
+@pytest.fixture
+def lista_de_tarefas_pendentes(db):
+    tarefas = [
+        Tarefa(nome='Tarefa1', feita=False),
+        Tarefa(nome='Tarefa2', feita=False),
+    ]
+    Tarefa.objects.bulk_create(tarefas)
+    return tarefas
+
+
+@pytest.fixture
+def resp_com_lista_de_tarefas(client, lista_de_tarefas_pendentes):
+    return client.get(reverse('tarefas:home'))
+
+
+def test_listar_de_tarefas_pendentes_presentes(client, resp_com_lista_de_tarefas, lista_de_tarefas_pendentes):
+    for tarefa in lista_de_tarefas_pendentes:
+        assertContains(resp_com_lista_de_tarefas, tarefa.nome)
