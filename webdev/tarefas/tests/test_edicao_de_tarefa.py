d0688644f9c27cb21f5c39a546161e68e9889be8
mateus <mateuslourenco55@outlook.com>
2022-01-26 00:39:19 -0300

    implementado botão para marcar tarefa como feita
    close #5

---
+++ webdev/tarefas/tests/test_edicao_de_tarefa.py
@@ -0,0 +1,26 @@
+import pytest
+from django.urls import reverse
+from pytest_django.asserts import assertContains
+
+from webdev.tarefas.models import Tarefa
+
+
+@pytest.fixture
+def tarefa_pendente(db):
+    return Tarefa.objects.create(nome='Tarefa1', feita=False)
+
+
+@pytest.fixture
+def resp_com_tarefa_pendente(client, tarefa_pendente):
+    return client.post(
+        reverse('tarefas:detalhe', kwargs={'tarefa_id': tarefa_pendente.id}),
+        data={'feita': 'true', 'nome': f'{tarefa_pendente.nome}-editada'}
+    )
+
+
+def test_status_code(resp_com_tarefa_pendente):
+    assert resp_com_tarefa_pendente.status_code == 302
+
+
+def test_tarefa_feita(resp_com_tarefa_pendente):
+    assert Tarefa.objects.first().feita
