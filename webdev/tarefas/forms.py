1523e69e6981233cd9590d84df68081e8473e61f
mateus <mateuslourenco55@outlook.com>
2022-01-29 21:43:59 -0300

    criada infra para criar usuario

--- webdev/tarefas/forms.py
+++ webdev/tarefas/forms.py
@@ -1,3 +1,6 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.forms import ModelForm
 
 from webdev.tarefas.models import Tarefa
@@ -13,3 +16,18 @@ class TarefaForm(ModelForm):
     class Meta:
         model = Tarefa
         fields = ['nome', 'feita']
+
+
+class NovoUsuario(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
+    def save(self, commit=True):
+        user = super(NovoUsuario, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
--- webdev/tarefas/urls.py
+++ webdev/tarefas/urls.py
@@ -4,6 +4,7 @@ from webdev.tarefas import views
 app_name = 'tarefas'
 urlpatterns = [
     path('', views.home, name='home'),
+    path('registrar', views.registrar, name='registrar'),
     path('<int:tarefa_id>', views.detalhe, name='detalhe'),
     path('apagar/<int:tarefa_id>', views.apagar, name='apagar'),
 ]
