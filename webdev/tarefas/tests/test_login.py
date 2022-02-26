1b598fa3a299d68becd442e31b5072f86ea434c2
Mateus Lourenço <mateuslourenco55@outlook.com>
2022-02-26 00:49:53 -0300

    atualizado model-mommy para model-bakery

--- webdev/tarefas/tests/test_login.py
+++ webdev/tarefas/tests/test_login.py
@@ -1,6 +1,6 @@
 import pytest
 from django.urls import reverse
-from model_mommy import mommy
+from model_bakery import baker
 
 from webdev.django_assertions import assert_contains, assert_not_contains
 
@@ -28,7 +28,7 @@ def test_link_registrar_disponivel(resp):
 
 @pytest.fixture
 def usuario(db, django_user_model):
-    usuario_modelo = mommy.make(django_user_model)
+    usuario_modelo = baker.make(django_user_model)
     senha = 'senha'
     usuario_modelo.set_password(senha)
     usuario_modelo.save()
