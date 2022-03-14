46971da99e50bdcb867233384497bb79c7bcff81
Mateus Lourenço <mateuslourenco55@outlook.com>
2022-03-13 22:16:30 -0300

    criado templates para confirmacao de reset de senha

--- webdev/settings.py
+++ webdev/settings.py
@@ -170,3 +170,12 @@ INTERNAL_IPS = config('INTERNAL_IPS', cast=Csv(), default='127.0.0.1')
 if DEBUG:
     INSTALLED_APPS.append('debug_toolbar')
     MIDDLEWARE.insert(0, "debug_toolbar.middleware.DebugToolbarMiddleware",)
+
+
+# Configuração de envio de email
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
